1e7ce4a6addfbe15a45e47d5d76b050098c6d146
ashwabu <abdulazizredi90@gmail.com>
2024-01-21 15:07:20 +0300

    Test creating a superuser.

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -19,3 +19,13 @@ class ModelTest(TestCase):
 
         self.assertEqual(user.username, username)
         self.assertTrue(user.check_password(password))
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'superuser',
+            'password123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
