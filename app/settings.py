cf093d19f806fa83a5de36b7347e5849b589cb87
ashwabu <abdulazizredi90@gmail.com>
2024-01-21 11:40:22 +0300

    mysql database

--- app/settings.py
+++ app/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'therapy-app-db',
+        'ENGINE': 'mysql.connector.django',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': 'password123',
     }
 }
 
