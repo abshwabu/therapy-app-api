3ad0d5169efdd2695039c531fd85057064410230
ashwabu <abdulazizredi90@gmail.com>
2024-01-21 14:03:43 +0300

    Test creating a user is successful.

--- core/tests/test_models.py
+++ core/tests/test_models.py
@@ -7,4 +7,15 @@ from django.contrib.auth import get_user_model
 
 class ModelTest(TestCase):
     """Test models."""
-    
+
+    def test_create_user_successful(self):
+        """Test creating a user is successful."""
+        username = 'username'
+        password = 'password123'
+        user = get_user_model().objects.create_user(
+            username=username,
+            password=password,
+        )
+
+        self.assertEqual(user.username, username)
+        self.assertTrue(user.check_password(password))
