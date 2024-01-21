976a3bad4a8e2c2a9c357d3a39d73499f9995f55
ashwabu <abdulazizredi90@gmail.com>
2024-01-21 15:12:06 +0300

    Create a new superuser.

--- core/models.py
+++ core/models.py
@@ -20,6 +20,15 @@ class UserManager(BaseUserManager):
 
         return user
 
+    def create_superuser(self, username, password):
+        """Create a new superuser."""
+        user = self.create_user(username, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
