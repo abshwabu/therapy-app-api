a6fa87a8a69242e2669a0ed982b464b0e9bc514f
ashwabu <abdulazizredi90@gmail.com>
2024-01-22 04:43:07 +0300

    "Define the admin interface for user.

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
+"""
+Django admin customization
+"""
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin interface for user."""
+    ordering = ['id']
+    list_display = ['username', 'email']
+
+admin.site.register(models.User, UserAdmin)
