55d21c4fe6cc32aba67ae9a95589f7c6d480d342
ashwabu <abdulazizredi90@gmail.com>
2024-01-23 01:03:46 +0300

    create and return a user with encrypted password.

--- user/serializers.py
+++ user/serializers.py
@@ -13,3 +13,8 @@ class UserSerializer(serializers.ModelSerializer):
         model = get_user_model()
         fields = ['username', 'password', 'email']
         extra_kwargs = {'password': {'write_only': True, 'min_length': 6}}
+
+    def create(self, validated_data):
+        """create and return a user with encrypted password."""
+        return get_user_model().objects.create_user(**validated_data)
+        
