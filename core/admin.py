84bea4ccd1c9e816f3d86cc09daa355eab3f6138
ashwabu <abdulazizredi90@gmail.com>
2024-01-22 05:03:25 +0300

    extra fields

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,7 @@ Django admin customization
 """
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
 from core import models
 
@@ -11,5 +12,20 @@ class UserAdmin(BaseUserAdmin):
     """Define the admin interface for user."""
     ordering = ['id']
     list_display = ['username', 'email']
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (
+            _('permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
 
 admin.site.register(models.User, UserAdmin)
