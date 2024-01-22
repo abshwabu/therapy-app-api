801143d062483198281b3a1d41ec1f74e692b14c
ashwabu <abdulazizredi90@gmail.com>
2024-01-22 14:47:50 +0300

    Tests for the User API.

---
+++ user/tests/test_user_api.py
@@ -0,0 +1,14 @@
+"""
+Tests for the User API.
+"""
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+
+def create_user(**params):
+    """Create a new user."""
+    return get_user_model().objects.create_user(**params)
