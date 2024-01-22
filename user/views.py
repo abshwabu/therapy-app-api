1aef337aad5b67188d534d476ee0efc6cd501192
ashwabu <abdulazizredi90@gmail.com>
2024-01-23 01:10:09 +0300

    Create a new user in the system.

--- user/views.py
+++ user/views.py
@@ -1,3 +1,12 @@
 """
 Views for the User API.
 """
+from rest_framework import generics
+
+from user.serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIViewModel):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+    
