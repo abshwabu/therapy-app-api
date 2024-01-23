3d6707a588136b0f6a5f111ac419fdd63083e3b1
ashwabu <abdulazizredi90@gmail.com>
2024-01-24 00:58:07 +0300

    localhost

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-scd(+hz#ejm)l06v!a)3n2&ap7c7^+cfgvjgux)hu=q+w4de^h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.2',]
+ALLOWED_HOSTS = ['10.0.2.2','127.0.0.1',]
 
 
 # Application definition
