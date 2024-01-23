3ed743884e5516dd0c99bbc6cfd603e55d859a38
ashwabu <abdulazizredi90@gmail.com>
2024-01-23 17:10:21 +0300

    Test that update user profile

--- user/tests/test_user_api.py
+++ user/tests/test_user_api.py
@@ -140,3 +140,14 @@ class PrivetUserApiTests(TestCase):
         res = self.client.post(ME_URL, {})
 
         self.assertEqual(res.status, status.HTTP_405_METHOD_NOT_ALLOWED)
+
+    def test_update_user_profile(self):
+        """Test that update user profile."""
+        payload = {'email': 'user2@example.com', 'password': 'password'}
+
+        res = self.client.patch(ME_URL, payload)
+
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.email, payload['email'])
+        self.assertTrue(self.user.check_password(payload['password']))
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
