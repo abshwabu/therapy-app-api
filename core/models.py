d616285f6951ceeda8e35c6c3f9e501e16c5813c
ashwabu <abdulazizredi90@gmail.com>
2024-01-27 07:23:00 +0300

    starting details model

--- core/models.py
+++ core/models.py
@@ -1,6 +1,7 @@
 """
 Database models.
 """
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -40,3 +41,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'username'
+
+
+class Details(models.Model):
+    """User details object."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    phone = models.IntegerField(null=True, blank=True, unique=True)
+    religion = models.TextChoices(null=True, blank=True, )
+    
+
