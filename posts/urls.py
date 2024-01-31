ed789f1dc0c563fb966472cb0fb89adeb47849ae
ashwabu <abdulazizredi90@gmail.com>
2024-01-31 21:42:11 +0300

    posts.urls

--- posts/urls.py
+++ posts/urls.py
@@ -7,3 +7,12 @@ from rest_framework.routers import DefaultRouter
 
 from posts import views
 
+
+router = DefaultRouter()
+router.register('posts', views.PostViewSet)
+
+app_name = 'post'
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
