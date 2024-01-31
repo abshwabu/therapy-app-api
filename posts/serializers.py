b6b63995995b1b1d304e82449fb0e78cb43d77cf
ashwabu <abdulazizredi90@gmail.com>
2024-01-31 20:32:08 +0300

    Serializer for posts

--- posts/serializers.py
+++ posts/serializers.py
@@ -6,3 +6,10 @@ from rest_framework import serializers
 from core.models import Post
 
 
+class PostSerializer(serializers.ModelSerializer):
+    """Serializer for posts"""
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title',]
+        read_only_fields = ['id']
