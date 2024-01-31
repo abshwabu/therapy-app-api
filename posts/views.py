b95e54e99188272471b6af1b88ec2d656a44d39c
ashwabu <abdulazizredi90@gmail.com>
2024-01-31 20:57:17 +0300

    imports

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,11 @@
 """
 Views for post APIs.
 """
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Post
+from posts import serializers
+
+
