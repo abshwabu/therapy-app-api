f5a0d87212e11ad7176e021b60b65b9ac5198811
ashwabu <abdulazizredi90@gmail.com>
2024-02-05 09:20:22 +0300

    Create a new post.

--- posts/serializers.py
+++ posts/serializers.py
@@ -20,13 +20,24 @@ class PostSerializer(serializers.ModelSerializer):
 
     tags = TagSerializer(many=True, required=False)
 
-
     class Meta:
         model = Post
         fields = ['id', 'title','tags']
         read_only_fields = ['id']
 
+    def create(self, validated_data):
+        """Create a new post."""
+        tags = validated_data.pop('tags', [])
+        post = Post.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag,
+            )
+            post.tags.add(tag_obj)
 
+        return post
 class PostDetailSerializer(PostSerializer):
     """serializer for post details."""
 
