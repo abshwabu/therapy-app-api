9eead79ec01051fcaa88c40a1de39e54de9012c4
ashwabu <abdulazizredi90@gmail.com>
2024-02-06 15:10:23 +0300

    static/static

--- app/settings.py
+++ app/settings.py
@@ -125,8 +125,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-MEDIA_URL = 'media/'
+STATIC_URL = 'static/static/'
+MEDIA_URL = 'static/media/'
+
+STATIC_ROOT = "/var/web/static/"
+MEDIA_ROOT = "/var/web/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
