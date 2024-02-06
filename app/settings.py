1c26b40ac50cdb9336ba1e1b93e53892a150e393
ashwabu <abdulazizredi90@gmail.com>
2024-02-06 15:41:53 +0300

    moving file

--- app/settings.py
+++ app/settings.py
@@ -129,8 +129,8 @@ USE_TZ = True
 STATIC_URL = 'static/static/'
 MEDIA_URL = 'static/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
